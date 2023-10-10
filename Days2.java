0b34d0a55412c5e1687a6656f6b3895264dc68f2
Indahh12 <indahanlar@gmail.com>
2023-10-10 13:41:13 +0800

    day 2

---
+++ Days2.java
@@ -0,0 +1,18 @@
+
+package days.pkg2;
+
+public class Days2 {
+
+        public static void main(String[] args) {
+            int angka = 3;
+            int angka2 = 2;
+            
+            
+            angka2 = angka2 - angka;
+            angka2 /= angka;
+            angka2 *=angka;
+            angka2 = angka2+angka;
+            System.out.println(angka2+angka);
+            }
+    
+}
