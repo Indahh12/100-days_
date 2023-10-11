776bad8af2c5de606114e2b3859ee284b1b55468
Indahh12 <indahanlar@gmail.com>
2023-10-11 14:29:44 +0800

    Day 3

---
+++ Day3.java
@@ -0,0 +1,20 @@
+package day.pkg3;
+public class Day3 {
+
+       public static void main(String[] args) {
+           
+           int angka1 = 6;
+           int angka2 = 4;
+           
+ //         boolean x = angka1 > angka2;
+  //        boolean x = angka1 < angka2;
+  //        boolean x = angka1 == angka2;
+          boolean x  = angka1!= angka2;
+           System.out.println(angka1 > angka2);
+           
+  
+  
+       
+    }
+    
+}
