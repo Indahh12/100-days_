07484a48a473f87252a79d60f77a5e7e7db912b1
Indahh12 <147224224+Indahh12@users.noreply.github.com>
2023-10-12 13:34:36 +0800

    Day4.java

--- Day4.java
+++ Day4.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package day.pkg4;
 
-/**
- *
- * @author asus1
- */
 public class Day4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Scanner masukan = new Scanner (System.in);
+        System.out.println ("Ketikkan nama:");
+        String nama = masukan.nextLine ();
+
+        System.out.println ("nama anda adalah " +nama);
+
+        
+
     }
     
 }
