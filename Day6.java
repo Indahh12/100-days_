6de7ab471fb19ca02c62e494c794f596bad2faed
Indahh12 <147224224+Indahh12@users.noreply.github.com>
2023-10-14 14:18:35 +0800

    Add files via upload

---
+++ Day6.java
@@ -0,0 +1,20 @@
+
+package day.pkg6;
+
+public class Day6 {
+
+      public static void main(String[] args) {
+          System.out.println("Masukkan tahun kelahiran anda");
+          
+          int tahunSekarang = 2023;
+          
+          int tahunKelahiran= 2005;
+          
+                    
+                    int umur = tahunSekarang - tahunKelahiran;
+          System.out.println("Umur anda adalah" + umur + "tahun");
+          
+          
+                      }
+    
+}
