d1fff95750bd5fdd05f0eebd907e65b950b807cc
Indahh12 <indahanlar@gmail.com>
2023-10-15 13:12:40 +0800

    Day 7

---
+++ Day7.java
@@ -0,0 +1,14 @@
+
+package day.pkg7;
+
+public class Day7 {
+
+        public static void main(String[] args) {
+            byte data1= 20;
+            byte data2= 30;
+            byte hasil = (byte)(data1 + data2);
+            
+            System.out.println("hasil penjumlahan : " + hasil);
+            }
+    
+}
