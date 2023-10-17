e9ff514d06c606bf11445aa27e776cbf108bc64c
Indahh12 <indahanlar@gmail.com>
2023-10-17 18:35:37 +0800

    Day 9

---
+++ Day9.java
@@ -0,0 +1,14 @@
+
+package day.pkg9;
+public class Day9 {
+        public static void main(String[] args) {
+            long nilai = 2000000000L ;
+            if ( nilai > 1000000000L)
+                System.out.println("Nilai lebih dari 1 miliar:");
+            else 
+                System.out.println("Nilai tidak lebih dari 1 miliar:");
+                
+       
+    }
+    
+}
