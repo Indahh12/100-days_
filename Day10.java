37f352012a6895443055220c82d0c21d14a2f93f
Indahh12 <indahanlar@gmail.com>
2023-10-18 18:10:57 +0800

    Day 10

---
+++ Day10.java
@@ -0,0 +1,16 @@
+
+package day.pkg10;
+public class Day10 {
+    public static void main(String[] args) {
+      double nilai1 = 4.567;
+      double nilai2 = 2.345;
+      double jumlah = nilai1 + nilai2;
+      
+        System.out.println("Angka 1: " + nilai1);
+        System.out.println("Angka2 : "+ nilai2);
+        
+        System.out.println("Hasil penjumlahan" + jumlah);
+                
+    }
+    
+}
