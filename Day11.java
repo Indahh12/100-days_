b6aac2139ddebed7943766116a6f1c844bcfa55d
Indahh12 <indahanlar@gmail.com>
2023-10-19 11:55:24 +0800

    Day 11

---
+++ Day11.java
@@ -0,0 +1,16 @@
+
+package day.pkg11;
+public class Day11 {
+        public static void main(String[] args) {
+            float nilai = 7.5f;
+                    if (nilai >= 7.0f)
+                        System.out.println("Anda lulus");
+                    else 
+                        System.out.println("Anda tidak lulus");
+                    
+                    
+            
+        
+    }
+    
+}
