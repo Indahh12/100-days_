eb50ddd7ada6860266d342b0df767920e843937a
Indahh12 <147224224+Indahh12@users.noreply.github.com>
2023-10-20 11:11:04 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -2,11 +2,13 @@
 package day.pkg11;
 public class Day11 {
         public static void main(String[] args) {
-            float nilai = 7.5f;
-                    if (nilai >= 7.0f)
-                        System.out.println("Anda lulus");
-                    else 
-                        System.out.println("Anda tidak lulus");
+            int i;
+                for (i=1; i<=100; i++)
+                        {
+                                System.out.println(i + "");
+                                        }
+                                       
+                        
                     
                     
             
