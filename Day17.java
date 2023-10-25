760f4cafd4fdd7d3542ea09d34d4542df79b457c
Indahh12 <indahanlar@gmail.com>
2023-10-25 16:39:10 +0800

    Day 17

---
+++ Day17.java
@@ -0,0 +1,18 @@
+
+package day.pkg17;
+public class Day17 {
+    public static void main(String[] args) {
+        double panjang = 6.0;
+            
+        double lebar = 3.0;
+        double tinggi = 2.0;
+        
+        
+        double volume = panjang * lebar * tinggi ;
+        
+        System.out.println("Volume balok adalah: " + volume);
+        
+        
+    }
+    
+}
