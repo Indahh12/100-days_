3a1a62ea95b06d3e68edcd5903248cd338c3fa28
Indahh12 <indahanlar@gmail.com>
2023-10-27 16:51:37 +0800

    Day 19

---
+++ Day19.java
@@ -0,0 +1,22 @@
+
+package day.pkg19;
+public class Day19 {
+
+    public static void main(String[] args) {
+      // contoh perulangan for untuk mencetak angka dari 1 hingga 10
+      for (int i = 1; i<= 10; i++) {
+                    System.out.println(i);
+      }
+      
+     // contoh perulangan for untuk mencetak angka genap dari 2 hingga 10
+     for (int j = 2; j <= 10; j += 2 ){
+                  System.out.println(j);
+     }
+     
+     //contoh perulangan for untuk mencetak angka dari 10 hingga 1 dengan langkah -1
+     for (int k = 10; k >= 1; k--) {
+                  System.out.println(k);
+     }
+    }
+    
+}
