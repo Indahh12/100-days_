ea717952dbc1ddbda6d168f5636712a4040744f4
Indahh12 <indahanlar@gmail.com>
2023-10-29 12:25:52 +0800

    Day 21

---
+++ Day21.java
@@ -0,0 +1,19 @@
+package day.pkg21;
+
+import java.util.Scanner;
+
+public class Day21 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner (System.in);
+        
+        //Masukkan jumlah hari
+        System.out.println("Masukkan jumlah hari: ");
+        int jumlahHari = input.nextInt();
+        
+        //Hitung jumlah bulan 
+        int jumlahBulan = jumlahHari / 30 ;
+        
+        System.out.println("Jumlah bulan dalam " + jumlahHari + "hari adalah: " + jumlahBulan + "bulan");
+    }
+    
+}
