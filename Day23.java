2735f927c4ddb047b857874276b1eb05ac5abf38
Indahh12 <indahanlar@gmail.com>
2023-10-31 16:43:47 +0800

    Day 23

---
+++ Day23.java
@@ -0,0 +1,42 @@
+package day.pkg23;
+
+public class Day23 {
+
+    
+    public static void main(String[] args) {
+        int angka1= 5;
+        int angka2 = 10;
+        
+        //Operator pembanding '=='
+        boolean sama = angka1 == angka2;
+        System.out.println("Apakah angka1 sama dengan angka2? = " + sama);
+        
+        //Operator pembanding '!='
+        boolean tidakSama = angka1 != angka2;
+        System.out.println("Apakah angka1 tidak sama dengan angka 2? = " + tidakSama);
+        
+        //Operator pembanding '>'
+        boolean lebihBesar = angka1 > angka2;
+        System.out.println("Apakah angka1 lebih besar dari angka2 ? = " + lebihBesar);
+        
+        //Operator prmbandingan '<'
+        boolean lebihKecil = angka1 < angka2 ;
+        System.out.println("Apakah angka1 lebih kecil dari angka2? = " + lebihKecil);
+        
+        //Operator pembanding '>='
+        boolean lebihBesarSamaDengan = angka1 >= angka2;
+        System.out.println("Apakah angka1 lebih besar  atau sama dengan angka 2? = " + lebihBesarSamaDengan);
+        
+        //Operator pembanding '<='
+        boolean lebihKecilSamaDengan = angka1 <= angka2;
+        System.out.println("Apakah angka1 lebin kecil atau sama dengan angka2? = " + lebihKecilSamaDengan);
+        
+        
+        
+                
+                
+        
+        
+    }
+    
+}
