9467b3b848cd0bf7a28b28fa40a342e70683de1d
Indahh12 <indahanlar@gmail.com>
2023-11-03 12:58:55 +0800

    Day 26

---
+++ Day26.java
@@ -0,0 +1,25 @@
+
+package day.pkg26;
+public class Day26 {
+    public static void main(String[] args) {
+       //Mendeklarasikan dan menginisialisasi sebuah array integer dengan panjang 5
+       int[] numbers = new int[5];
+       
+       //Mengisi elemen-elemen array 
+       numbers [0] = 10;
+       numbers [1] = 20;
+       numbers [2] = 30;
+       numbers [3] = 40;
+       numbers [4] = 50;
+       
+       //Mengakses elemen-elemen array dan mencetaknya
+        System.out.println("Elemen-elemen dalam array numbers adalah : ");
+        System.out.println("Element 0: " + numbers [0]);
+        System.out.println("Element 1: " + numbers [1]);
+        System.out.println("Element 2: " + numbers [2]);
+        System.out.println("Element 3: " + numbers [3]);
+        System.out.println("Element 4: " + numbers [4]);
+         
+    }
+    
+}
