9272a327f6951c7cee2f6cbb99262955cecf0544
Indahh12 <indahanlar@gmail.com>
2023-11-04 12:58:54 +0800

    Day 27

---
+++ Day27.java
@@ -0,0 +1,15 @@
+
+package day.pkg27;
+
+public class Day27 {
+    public static void main(String[] args) {
+       //Membuat array integer dengan 5 elemen 
+       int [] numbers = {1, 2, 3, 4, 5};
+       
+       //Mengakses elemen-elemen array dan mencetaknya
+       for (int i=0; i < numbers.length; i++){
+           System.out.println("Element ke-" + i + ": " + numbers[i]);
+       }
+    }
+    
+}
