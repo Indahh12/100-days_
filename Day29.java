75a9428f0fa6af2b7c4d78a72e64a68f54df0290
Indahh12 <indahanlar@gmail.com>
2023-11-06 14:43:53 +0800

    Day 29

---
+++ Day29.java
@@ -0,0 +1,11 @@
+
+package day.pkg29;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        String [] nama = { "Indah", "Silfi", "Siti", "Febri"}; 
+        System.out.println(nama [0]);
+    }
+    
+}
