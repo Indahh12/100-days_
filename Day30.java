602528b8967194ba550a10176f46e601b42235db
Indahh12 <indahanlar@gmail.com>
2023-11-07 18:39:24 +0800

    Day 30

---
+++ Day30.java
@@ -0,0 +1,18 @@
+
+package day.pkg30;
+public class Day30 {
+    public static void main(String[] args) {
+        //Membuat array untuk menyimpan nama
+        String[] nama = {"Indah", "Indri", "Lukman", "Sukman"};
+        
+        //Membuat array untuk menyimpan umur
+        int[] umur = {18, 10, 22, 21};
+        
+       //Menampilkan nama dan umur
+       for (int i = 0; i < nama.length; i++){
+           System.out.println("Nama: " + nama[i] + ", Umur: " + umur[i] );
+       }
+           
+    }
+    
+}
