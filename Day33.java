bf9080347cc9d03c1a2929355e6c71752f9bf281
Indahh12 <indahanlar@gmail.com>
2023-11-10 18:56:06 +0800

    Day 33

---
+++ Day33.java
@@ -0,0 +1,17 @@
+
+package day.pkg33;
+
+public class Day33 {
+    public static void main(String[] args) {
+        // menggunakan foor loop
+        for (int i =1 ; i<=100; i++){
+            if (i%2==0)
+                
+            System.out.println("genap" + i);
+            
+            
+            
+        }
+    }
+    
+}
