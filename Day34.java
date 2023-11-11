4757984372f13d9adce5c9b94188268277409621
Indahh12 <indahanlar@gmail.com>
2023-11-11 09:38:13 +0800

    Day 34

---
+++ Day34.java
@@ -0,0 +1,17 @@
+
+package day.pkg34;
+
+public class Day34 {
+
+
+    public static void main(String[] args) {
+        //Menggunakan while loop
+        int angka = 1;
+        
+        while (angka <= 100){
+            System.out.println(angka);
+            angka += 2;
+        }
+    }
+    
+}
