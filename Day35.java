f6ea3028ecf0e527d9c29717c7f377e50bff4218
Indahh12 <indahanlar@gmail.com>
2023-11-12 11:42:26 +0800

    Day 35

---
+++ Day35.java
@@ -0,0 +1,16 @@
+
+package day.pkg35;
+
+public class Day35 {
+
+    public static void main(String[] args) {
+        //Menggunakan do-while loop 
+        int k = 1;
+        do{
+            System.out.println(k);
+            k++;
+        }while (k<= 100);
+        }
+    }
+    
+
