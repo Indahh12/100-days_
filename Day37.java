0a655dea3a0d4615b6b5fc9354ac88dcf2dc85bf
Indahh12 <indahanlar@gmail.com>
2023-11-14 19:25:10 +0800

    Day 37

---
+++ Day37.java
@@ -0,0 +1,36 @@
+
+package day.pkg37;
+public class Day37 {
+    public static void main(String[] args) {
+        //Operator penugasan
+        int a = 10;
+        int b = 12;
+        int c = 18;
+        int d = 15;
+        int e = 16;
+        
+        a += 3;
+        b -= 5;
+        c *= 3;
+        d /= 3;
+        e %= 4;
+        
+        System.out.println("nilai a: " + a);
+        System.out.println("nilai b: " + b);
+        System.out.println("nilai c: " + c);
+        System.out.println("nilai d: " + d);
+        System.out.println("nilai e: " + e);
+        
+}
+    }
+        
+
+               
+            
+                      
+        
+        
+        
+    
+    
+
