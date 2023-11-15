3fca6863d1a6eb70fa8ae58c0ae2f7f2023d2648
Indahh12 <indahanlar@gmail.com>
2023-11-15 18:25:14 +0800

    Day 38

---
+++ Day38.java
@@ -0,0 +1,14 @@
+
+package day.pkg39;
+public class Day39 {
+    public static void main(String[] args) {
+        long nilai = 8000000000L; //Menetapkan nilai long
+        
+        if (nilai > 1000000000L){
+            System.out.println("Nilai lebih dari 1 miliar");
+        }else{
+            System.out.println("Nilai tidak lebih dari 1 miliar");
+        }
+    }
+    
+}
