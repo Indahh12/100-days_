cafa8945edd05e4b340f27dcc85581325c99aa97
Indahh12 <indahanlar@gmail.com>
2023-11-17 21:07:59 +0800

    Day 40

---
+++ Day40.java
@@ -0,0 +1,21 @@
+
+package day.pkg40;
+
+
+public class Day40 {
+
+    
+    public static void main(String[] args) {
+    //Contoh penggunaan operator &&
+    int a = 10;
+    int b = 15;
+    
+    if (a > 0 && b > 0){
+        System.out.println("Kedua nilai positif");
+    }else{
+        System.out.println("Salah satu atau kedua nilai tidak positif");
+    }
+
+    }
+    
+}
