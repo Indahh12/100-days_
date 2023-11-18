fbb67a15b36aa6c92237040132a5af35ab17988e
Indahh12 <indahanlar@gmail.com>
2023-11-18 20:41:26 +0800

    Operator || pada kondisi if

---
+++ Day41.java
@@ -0,0 +1,23 @@
+
+package day.pkg41;
+
+
+public class Day41 {
+
+    
+    public static void main(String[] args) {
+        //Operator || pada kondisi if
+        
+        int angka1 = 5;
+        int angka2 = 10;
+        
+        if (angka1 >= 0 || angka2 <= 5){
+            System.out.println("Salah satu kondisi terpenuhi");
+        }else{
+            System.out.println("Kedua kondisi tidak terpenuhi");
+            
+        }
+        
+    }
+    
+}
