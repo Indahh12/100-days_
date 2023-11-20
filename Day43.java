7e3b007b1d61fbf2eac93552a6e90e019974f6d5
Indahh12 <indahanlar@gmail.com>
2023-11-20 13:42:52 +0800

    Bilangan ganjil 1 sampai 99

---
+++ Day43.java
@@ -0,0 +1,12 @@
+
+package day.pkg43;
+public class Day43 {
+       public static void main(String[] args) {
+        for(int ganjil = 1; ganjil <= 99; ganjil+=2) {
+        System.out.println(ganjil);
+            
+        }
+        
+    }
+    
+}
