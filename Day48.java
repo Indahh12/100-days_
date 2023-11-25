e581a8dc297005f4a4e89d119efeeba6f4389377
Indahh12 <indahanlar@gmail.com>
2023-11-25 13:28:08 +0800

    pola segitiga

---
+++ Day48.java
@@ -0,0 +1,14 @@
+
+package day.pkg48;
+public class Day48 {
+
+    public static void main(String[] args) {
+       for (int i = 1; i<= 5; i++){
+           for (int j =1; j<=i; j++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+    }
+    
+}
