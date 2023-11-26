2ae86fb3114c42b7f013c34cdfb9fb3a07239115
Indahh12 <indahanlar@gmail.com>
2023-11-26 11:50:45 +0800

    Pola persegi

---
+++ Day49.java
@@ -0,0 +1,15 @@
+
+package day.pkg49;
+public class Day49 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
