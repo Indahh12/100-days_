18c7fa0f85cf51d0be6ffc05c2f7f4544f677eae
Indahh12 <indahanlar@gmail.com>
2023-11-27 21:20:09 +0800

    Break

---
+++ Day50.java
@@ -0,0 +1,18 @@
+
+package day.pkg50;
+
+public class Day50 {
+
+    public static void main(String[] args) {
+       //Break
+       
+       for (int w =0; w<7; w++){
+           if (w == 4){
+               continue;
+           }else{
+               System.out.print(w);
+           }
+       }
+    }
+    
+}
