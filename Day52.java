62dbdd119d52711608091c0c8e5b1f209b9028b8
Indahh12 <indahanlar@gmail.com>
2023-11-29 21:07:14 +0800

    Cetak isi array dengan while

---
+++ Day52.java
@@ -0,0 +1,16 @@
+
+package day.pkg52;
+
+public class Day52 {
+
+    public static void main(String[] args) {
+       int[] angka = {1,2,3,4};
+       
+       int i = 0;
+       while (i < angka.length){
+           System.out.println(angka[i]);
+           i++;
+       }
+    }
+    
+}
