96efe07176def8865044b5eb60ded76123835291
Indahh12 <indahanlar@gmail.com>
2023-11-30 20:07:10 +0800

    Cetal isi array dengan do-while

---
+++ Day53.java
@@ -0,0 +1,15 @@
+
+package Day53;
+
+public class Day53 {
+    public static void main(String[] args) {
+       int [] nilai = {12,22,42,100};
+       
+       int i = 0;
+       do {
+           System.out.println(nilai[i]);
+           i++;
+       } while (i<nilai.length);
+           
+    }
+}
