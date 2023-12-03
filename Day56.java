ea0ca2beea230f41b463052e55d490accb03da6b
Indahh12 <indahanlar@gmail.com>
2023-12-03 20:58:12 +0800

    Operator ternary

---
+++ Day56.java
@@ -0,0 +1,17 @@
+
+package day.pkg56;
+
+public class Day56 {
+
+    public static void main(String[] args) {
+       int angka = 10;
+
+//        penggunaan operator ternary
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+
+
+        System.out.println("Angka ini adalah " + hasil);
+
+    }
+    
+}
