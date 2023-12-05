c3b0c87a556c4c8f33f3b0f10b301fc265d147d4
Indahh12 <indahanlar@gmail.com>
2023-12-05 18:40:46 +0800

    Menjumlahkan array

---
+++ Day58.java
@@ -0,0 +1,18 @@
+
+package day.pkg58;
+
+public class Day58 {
+
+    public static void main(String[] args) {
+         int angka[] = {31,23,34,46,57,89,92};
+
+        int jumlah = 0;
+
+        for (int i = 0; i < angka.length; i++) {
+            jumlah += angka[i];
+        }
+            System.out.println("Jumlah keseluruhan Array adalah " +jumlah);
+
+    }
+    
+}
