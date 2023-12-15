695c364cdbcbba87738d3e2c8ce9e4773df6dfd8
Indahh12 <indahanlar@gmail.com>
2023-12-15 17:52:24 +0800

    Menentukan nilai negatif dalam array

---
+++ Day68.java
@@ -0,0 +1,18 @@
+
+package day.pkg68;
+
+public class Day68 {
+
+    public static void main(String[] args) {
+      int []nilai={2, 4, -15, 72, 80, -3, 8};
+int count=0;
+        System.out.println("nilai negatif dalam array : ");
+        for (int i = 0; i <nilai.length; i++) {
+            if(nilai[i]<0){
+                System.out.println(nilai[i]);
+            }
+
+    }
+    }
+    
+}
