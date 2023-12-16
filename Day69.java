e72c847866962c3b71b2794b10e28278883f287a
Indahh12 <indahanlar@gmail.com>
2023-12-16 23:15:46 +0800

    Cetak isi Array dengan for-each dan kelas Array

---
+++ Day69.java
@@ -0,0 +1,27 @@
+
+package day.pkg69;
+
+import java.util.Arrays;
+
+public class Day69 {
+
+    public static void main(String[] args) {
+         
+        System.out.println("mencetak isi array dengan for-each dan kelas Arrays");
+        int [][] nilai = {
+            {23,11,-9,10},
+            {4,13,6,0}
+        };
+        System.out.println("Menggunakan for-each");
+        for (int [] baris : nilai) {
+            for (int elemen : baris) {
+                System.out.print(elemen + " ");
+            }
+            System.out.println();
+           
+        }
+        System.out.println("Menggunakan kelas Arrays");
+        System.out.println(Arrays.deepToString(nilai));
+          }
+    
+}
