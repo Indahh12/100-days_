08cfadbb74bff9f7ce8c816f6b7ab065ed29a012
Indahh12 <indahanlar@gmail.com>
2023-12-28 20:56:54 +0800

    Kode barang (switch case)

---
+++ Day81.java
@@ -0,0 +1,46 @@
+
+package day.pkg81;
+
+import java.util.Scanner;
+
+public class Day81 {
+
+    public static void main(String[] args) {
+         /* buat program yang meminta user memasukan kode barang
+           yang akan di kirim ke alamat yang sudah di tentukan. */
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Masukan kode barang yang akan di kirim : ");
+        int kodeBarang = sc.nextInt();
+
+        String alamatTujuan;
+
+        switch(kodeBarang){
+            case 123:
+                alamatTujuan = "JL. MAWAR NO. 02, JAKARTA";
+                System.out.println("Barang dengan kode " + kodeBarang);
+                System.out.println("akan dikirim ke alamat " + alamatTujuan);
+                break;
+            case 345:
+                alamatTujuan = "JL. INDAH NO. 07, JAKARTA";
+                System.out.println("Barang dengan kode " + kodeBarang);
+                System.out.println("akan dikirim ke alamat " + alamatTujuan);
+                break;
+            case 567:
+                alamatTujuan = "JL. MELATI NO. 12, JAKARTA";
+                System.out.println("Barang dengan kode " + kodeBarang);
+                System.out.println("akan dikirim ke alamat " + alamatTujuan);
+                break;
+            case 789:
+                alamatTujuan = "JL. JAGOAN NO. 15, JAKARTA";
+                System.out.println("Barang dengan kode " + kodeBarang);
+                System.out.println("akan dikirim ke alamat " + alamatTujuan);
+                break;
+            default:
+                System.out.println("ANDA SALAH MEMASUKAN KODE BARANG !");
+                break;
+        }
+    }
+    
+}
