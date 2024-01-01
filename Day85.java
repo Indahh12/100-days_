9e0c519c96137b15e85dbb1f3ea8bfbc9a8f59e6
Indahh12 <indahanlar@gmail.com>
2024-01-01 20:07:55 +0800

    Mengubah isi array

---
+++ Day85.java
@@ -0,0 +1,15 @@
+
+package day.pkg85;
+
+import java.util.Arrays;
+
+public class Day85 {
+    public static void main(String[] args) {
+        
+        String warna [] = {"biru","ungu","coklat"};
+
+        warna [2] = "hijau";
+
+        System.out.println(Arrays.toString(warna));
+    }
+}
