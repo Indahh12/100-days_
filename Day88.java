ac6eb46e98dc6c95a4dfb8910f4393abd37035c2
Indahh12 <indahanlar@gmail.com>
2024-01-04 19:30:16 +0800

    Mengurutkan array sesuai abjad

---
+++ Day88.java
@@ -0,0 +1,18 @@
+
+package day.pkg88;
+
+import java.util.Arrays;
+
+public class Day88 {
+
+    public static void main(String[] args) {
+          String[] buah = {"Manggis","Jeruk","Anggur","Durian","Cherry","Berry"};
+        Arrays.sort(buah);
+        int i = 0;
+        while(i < buah.length){
+            System.out.println(buah[i]);
+            i++;
+    }
+    
+}
+}
