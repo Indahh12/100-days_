272710f791703fb21d7058433aff99b38483edc6
Indahh12 <indahanlar@gmail.com>
2024-01-05 19:04:04 +0800

    Mengurutkan array sesuai nilai

---
+++ Day89.java
@@ -0,0 +1,18 @@
+
+package day.pkg89;
+
+import java.util.Arrays;
+
+public class Day89 {
+
+    public static void main(String[] args) {
+         int [] nilai = {1, 44, 82, 100, 68, 99,12};
+        Arrays.sort(nilai);
+        int i = 0;
+        while(i < nilai.length){
+            System.out.println(nilai[i]);
+            i++;
+        }
+    }
+    
+}
