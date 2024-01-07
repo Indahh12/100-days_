10d1f8ea9b796a88ca6c64834f5b7bc2fdad312b
Indahh12 <indahanlar@gmail.com>
2024-01-07 20:42:50 +0800

    Cetak array terbalik

---
+++ Day91.java
@@ -0,0 +1,12 @@
+
+package day.pkg91;
+public class Day91 {
+    public static void main(String[] args) {
+        String hewan [] = {"Singa","Buaya","Beruang","Macan tutul"};
+
+        for (int i = hewan.length-1; i >= 0; i--) {
+            System.out.println("indeks ke " + i + " = " + hewan[i]); 
+        }
+    }
+    
+}
