fd59af4526b72522314fdf96a0c10af40f08b63e
Indahh12 <indahanlar@gmail.com>
2024-01-11 20:25:34 +0800

    Memanggil method prosedur

---
+++ Day95.java
@@ -0,0 +1,20 @@
+
+package day.pkg95;
+
+public class Day95 {
+ public static void mahasiswa(){
+        String nama = "Indah";
+        int umur = 18;
+        String kelas = "informatika B";
+        System.out.println("Nama \t: " + nama );
+        System.out.println("Umur \t: " + umur + " tahun");
+        System.out.println("Kelas \t: " + kelas );
+    }
+
+    public static void main(String[] args) {
+      mahasiswa(); 
+    
+       
+    }
+    
+}
