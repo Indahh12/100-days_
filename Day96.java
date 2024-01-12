3eb982d2d60a828007b714b9c5efe80a23851809
Indahh12 <indahanlar@gmail.com>
2024-01-12 20:24:30 +0800

    Memanggil method fungsi

---
+++ Day96.java
@@ -0,0 +1,17 @@
+
+package day.pkg96;
+
+public class Day96 {
+ public static int nilai(){
+        int a = 2;
+        int b = 24;
+        int c = 6;
+        return a+b+c;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(nilai());
+   
+    }
+    
+}
