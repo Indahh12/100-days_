8125fe97e4eade6eff9f41bbf988e32374d047bc
Indahh12 <indahanlar@gmail.com>
2024-01-14 20:26:36 +0800

    Method parameter, Aritmatika

---
+++ Day98.java
@@ -0,0 +1,18 @@
+
+package day.pkg98;
+public class Day98 {
+  public static void aritmatika(int a, int b){
+        System.out.println(a + " + " + b + " = " + (a+b));
+        System.out.println(a + " - " + b + " = " + (a-b));
+        System.out.println(a + " x " + b + " = " + (a*b));
+        System.out.println(a + " : " + b + " = " + (a/b));
+
+    }
+
+    public static void main(String[] args) {
+       aritmatika(30,10);
+    
+       
+    }
+    
+}
